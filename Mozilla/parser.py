26e1120b911577abda24ebf760ec52cf9466ee8a
Bilal Can <canbil@itu.edu.tr>
2019-06-19 12:51:32 +0300

    started to parse Moizilla xml files

---
+++ Mozilla/parser.py
@@ -0,0 +1,34 @@
+#@Author: Bilal CAN
+
+from xml.etree import ElementTree as ET
+import csv
+import os
+import datetime
+
+with open('Resources/bugzilla_assigned_to.xml','rt') as bugzilla_dev:
+    bugzilla_dev_tree = ET.parse(bugzilla_dev)
+with open('Resources/bugzilla_resolution.xml','rt') as bugzilla_res:
+    bugzilla_res_tree = ET.parse(bugzilla_res)
+with open('Resources/bugzilla_short_desc.xml','rt') as bugzilla_desc:
+    bugzilla_desc_tree = ET.parse(bugzilla_desc)
+
+with open('Resources/core_assigned_to.xml','rt') as core_dev:
+    core_dev_tree = ET.parse(core_dev)
+with open('Resources/core_resolution.xml','rt') as core_res:
+    core_res_tree = ET.parse(core_res)
+with open('Resources/core_short_desc.xml','rt') as core_desc:
+    core_desc_tree = ET.parse(core_desc)
+
+with open('Resources/firefox_assigned_to.xml','rt') as firefox_dev:
+    firefox_dev_tree = ET.parse(firefox_dev)
+with open('Resources/firefox_resolution.xml','rt') as firefox_res:
+    firefox_res_tree = ET.parse(firefox_res)
+with open('Resources/firefox_short_desc.xml','rt') as firefox_desc:
+    firefox_desc_tree = ET.parse(firefox_desc)
+
+with open('Resources/thunderbird_assigned_to.xml','rt') as thunderbird_dev:
+    thunderbird_dev_tree = ET.parse(thunderbird_dev)
+with open('Resources/thunderbird_resolution.xml','rt') as thunderbird_res:
+    thunderbird_res_tree = ET.parse(thunderbird_res)
+with open('Resources/thunderbird_short_desc.xml','rt') as thunderbird_desc:
+    thunderbird_desc_tree = ET.parse(thunderbird_desc)
