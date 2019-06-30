850d752430d4b4ce9bd41d4baa83497e628f29c8
Bilal Can <canbil@itu.edu.tr>
2019-06-30 14:23:09 +0300

    tokenizer of topic modelling in eclipse.py is generalized in a function for the sake of clean code

--- eclipse.py
+++ eclipse.py
@@ -9,6 +9,23 @@ from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
+def tokenizer(texts):
+    tokens = []
+    p_stemmer = PorterStemmer()
+    tokenizer = RegexpTokenizer(r'\w+')
+    en_stop = set(stopwords.words('english')) #stopwords
+    en_stop.add('eclipse')
+    en_stop.add('error')
+    en_stop.add('project')
+    for element in range(len(texts)):
+        sub_tokens = []
+        for word in range(len(texts[element])):
+            sub_tokens += tokenizer.tokenize(texts[element][word]) #atomic words are gathered from each word
+            stopped_sub_tokens = [i for i in sub_tokens if not i in en_stop] #stopwords are discarded
+            stemmed_sub_tokens = [p_stemmer.stem(i) for i in stopped_sub_tokens] #stemming
+        tokens += [stemmed_sub_tokens]
+    return tokens
+
 if __name__ == "__main__":
     texts = []
     with open('Results/collected_data.csv', 'r') as collected_data:
@@ -21,20 +38,8 @@ if __name__ == "__main__":
             else:
                count_line += 1
                texts += [row[3].strip().split()]
-    en_stop = set(stopwords.words('english')) #stopwords
-    en_stop.add('eclipse')
-    en_stop.add('error')
-    en_stop.add('project')
-    p_stemmer = PorterStemmer()
-    tokenizer = RegexpTokenizer(r'\w+')
-    tokens = []
-    for element in range(len(texts)):
-        sub_tokens = []
-        for word in range(len(texts[element])):
-            sub_tokens += tokenizer.tokenize(texts[element][word]) #atomic words are gathered from each word
-            stopped_sub_tokens = [i for i in sub_tokens if not i in en_stop] #stopwords are discarded
-            stemmed_sub_tokens = [p_stemmer.stem(i) for i in stopped_sub_tokens] #stemming
-        tokens += [stemmed_sub_tokens]
+
+    tokens = tokenizer(texts)
 
     dict_of_tokens = corpora.Dictionary() #Dictionary object is created
     corpus_of_tokens = [dict_of_tokens.doc2bow(doc, allow_update = True) for doc in tokens] #corpus is created
